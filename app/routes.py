9064263fa442fcfce244ee014cc0c4d408423115
ValeraFCDM <leralazareva@gmail.com>
2025-08-04 13:44:56 +0300

    Flask. Практикум 1.

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,24 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 
-@app.route("/")
-def form():
-    return render_template("form.html")
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route("/submit", methods=["POST", "GET"])
-def submit():
-    if request.method == "POST":
-        name = request.form.get("name")
-        email = request.form.get("email")
-        color = request.form.get("color")
-        profession = request.form.get("profession")
-        hobbies = request.form.getlist("hobbies")
-        level = request.form.get("level")
-        return render_template(
-            "result.html",
-            name=name, email=email, color=color, profession=profession, hobbies=hobbies, level=level
-        )
-    else:
-        return redirect(url_for("form"))
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def form():
+    return render_template('contact.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        return render_template('result.html')
+
+    return redirect(url_for('form'))
