95c914f0d1e0e301672cee93d084cfb73cf63b29
ValeraFCDM <leralazareva@gmail.com>
2025-08-06 14:06:18 +0300

    Flask. Урок 9. Основы шаблонизации

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,32 @@
 from flask import render_template, request, redirect, url_for
+from datetime import datetime
 from app import app
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    current_datetime = datetime.now()
+    return render_template('home.html', current_datetime=current_datetime)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template('about.html', team_members=team_members)
 
 @app.route('/contact')
 def form():
-    return render_template('contact.html')
+    contact_manager = {
+        'name': 'Ivan A.',
+        'address': {
+            'street': 'Qwerty Street',
+            'city': 'MeowLand',
+            'house_num': '152'
+        }
+    }
+    return render_template('contact.html', contact_manager=contact_manager)
 
 @app.route('/submit', methods=['POST', 'GET'])
 def result():
